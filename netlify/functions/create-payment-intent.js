30daee184f8153e67b0afcabb97697404c9a313a
csokakristof <csokakristof03@gmail.com>
2026-06-03 12:24:50 +0200

    Update create-payment-intent.js

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,1 +1,16 @@
+const Stripe = require('stripe');
 
+exports.handler = async (event) => {
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const { amount } = JSON.parse(event.body);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency: 'huf',
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+  };
+};
